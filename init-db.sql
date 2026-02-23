98d5672683c40ffe3a3e8d79d75cd0480bdec2b8
nguyenchivi001 <nguyenchivi292004@gmail.com>
2026-02-23 15:50:09 +0700

    Create Dockerfile and docker-compose.yml

---
+++ init-db.sql
@@ -0,0 +1,5 @@
+-- Tạo database riêng cho từng service
+CREATE DATABASE IF NOT EXISTS auth_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS job_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS application_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS profile_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
