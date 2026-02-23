dbc37be783b202a860c7fc0d117a7cf8d7b89bcb
nguyenchivi001 <nguyenchivi292004@gmail.com>
2026-02-23 15:38:51 +0700

    Init commit

---
+++ job-service/src/test/java/com/jobboard/job_service/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobboard.job_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
