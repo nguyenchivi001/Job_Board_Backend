847b08283e6fd4efb22a410b7c5908642b9eece9
nguyenchivi001 <nguyenchivi292004@gmail.com>
2026-02-24 10:43:19 +0700

    Create entity User

---
+++ auth-service/src/main/java/com/jobboard/auth_service/entity/User.java
@@ -0,0 +1,39 @@
+package com.jobboard.auth_service.entity;
+
+import com.jobboard.auth_service.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
