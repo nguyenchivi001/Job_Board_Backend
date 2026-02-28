fda0454e619230568c2c27024d6371bb3118bc03
nguyenchivi001 <nguyenchivi292004@gmail.com>
2026-02-28 10:15:15 +0700

    Feat(job-service): add JobCategory enum

---
+++ job-service/src/main/java/com/jobboard/job_service/enums/JobCategory.java
@@ -0,0 +1,15 @@
+package com.jobboard.job_service.enums;
+
+public enum JobCategory {
+    IT,
+    MARKETING,
+    FINANCE,
+    DESIGN,
+    SALES,
+    ENGINEERING,
+    HR,
+    CUSTOMER_SERVICE,
+    EDUCATION,
+    HEALTHCARE,
+    OTHER
+}
